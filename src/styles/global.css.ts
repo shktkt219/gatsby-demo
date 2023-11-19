90676f30a72a54e002ece13deb307e44512c97af
shktkt219 <shktkt219@gmail.com>
2023-11-19 20:41:55 +0100

    edited fonts css

--- src/styles/global.css.ts
+++ src/styles/global.css.ts
@@ -1,4 +1,16 @@
-import { globalStyle } from "@vanilla-extract/css";
+import { globalStyle, globalFontFace } from "@vanilla-extract/css";
+
+const robotoSlab = "Roboto Slab"
+const notoSans = "Noto Sans";
+
+globalFontFace(robotoSlab, {
+  src: `url(https://fonts.gstatic.com/s/robotoslab/v34/BngbUXZYTXPIvIBgJJSb6s3BzlRRfKOFbvjojISmb2RjV9Su1cai.woff2) format('woff2')`,
+  fontWeight: 400
+});
+
+globalFontFace(notoSans, {
+  src: `url(https://fonts.gstatic.com/s/notosans/v34/o-0IIpQlx3QUlC5A4PNr5TRASf6M7Q.woff2) format('woff2')`
+});
 
 globalStyle(`html, body`, {
   height: `100%`,
@@ -8,4 +20,12 @@ globalStyle(`html, body`, {
 
 globalStyle(`h1, h3, h4, h5, h6`, {
   fontFamily: `Roboto, sans-serif`
+})
+
+globalStyle(`h2`, {
+  fontFamily: robotoSlab
+})
+
+globalStyle(`p`, {
+  fontFamily: notoSans
 })
